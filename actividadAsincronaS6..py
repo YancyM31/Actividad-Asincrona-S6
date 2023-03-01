2ae0c520cbfa04d69ec957a38c1984e626a23297
YancyM31 <126370497+YancyM31@users.noreply.github.com>
2023-02-28 22:49:13 -0600

    Add files via upload

---
+++ actividadAsincronaS6..py
@@ -0,0 +1,49 @@
+print ("SUMA DE LAS VARIABLES")
+a= 20
+b= 15
+c= 10
+suma= a + b + c
+print ("La suma de las tres variables es: ", suma)
+print ("")
+print ("RESTA DE LAS VARIABLES")
+uno= 8
+dos= 14
+tres= 19
+cuatro= 17
+resta= uno - dos - tres - cuatro
+print ("La resta de las cuatro variables es: ", resta)
+print ("")
+print ("MULTIPLICACIÓN DE LA VARIABLE A POR LA VARIABLE B")
+A= 6
+B= 5
+variableC= A * B
+print ("Nos da como restado la Variable C que es: ", variableC)
+print ("")
+resultadoSuma= variableC * b
+print ("La Variable C multiplicada por la segunda variable de la suma es: ", resultadoSuma)
+print ("")
+resultadoResta= variableC* dos
+print  ("La Variable C multiplicada por la segunda variable de la resta es: ", resultadoResta)
+print ("")
+
+print ("VARIABLES EXPONENCIALES")
+variableExponencial= A ** B
+print ("El resultado de A elevado a la B es: ", variableExponencial)
+print ("")
+print ("MÓDULO")
+a
+uno
+resultadoMódulo= a % uno
+print ("El rsultado del módulo es: ", resultadoMódulo)
+print ("")
+print ("DIVISIÓN")
+variableExponencial
+resultadoMódulo
+total= variableExponencial / resultadoMódulo
+print("El resultado de la División es: ", total)
+print("")
+print("DIVISIÓ ENTERA")
+totalDivEntera= variableExponencial // resultadoMódulo
+print ("El resultado de la división entera es: ", totalDivEntera)
+
+
